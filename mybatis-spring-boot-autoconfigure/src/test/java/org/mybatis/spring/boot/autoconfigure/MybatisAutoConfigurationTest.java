248fd146c4384be637e28929167f11127d137333
Kazuki Shimizu <kazuki43zoo@gmail.com>
2016-03-04 01:23:56 +0900

    Add assertion for executor type

--- mybatis-spring-boot-autoconfigure/src/test/java/org/mybatis/spring/boot/autoconfigure/MybatisAutoConfigurationTest.java
+++ mybatis-spring-boot-autoconfigure/src/test/java/org/mybatis/spring/boot/autoconfigure/MybatisAutoConfigurationTest.java
@@ -90,7 +90,7 @@ public class MybatisAutoConfigurationTest {
 		assertEquals(1,
 				this.context.getBeanNamesForType(SqlSessionTemplate.class).length);
 		assertEquals(1, this.context.getBeanNamesForType(CityMapper.class).length);
-		assertEquals(ExecutorType.SIMPLE, this.context.getBean(SqlSessionTemplate.class).getExecutorType());
+		assertEquals(ExecutorType.SIMPLE, this.context.getBean(SqlSessionTemplate.class).getExecutorType()); // use a default executor type(SIMPLE)
 	}
 
 	@Test
@@ -103,6 +103,7 @@ public class MybatisAutoConfigurationTest {
 		this.context.refresh();
 		assertEquals(1, this.context.getBeanNamesForType(SqlSessionFactory.class).length);
 		assertEquals(1, this.context.getBeanNamesForType(CityMapperImpl.class).length);
+		assertEquals(ExecutorType.BATCH, this.context.getBean(SqlSessionTemplate.class).getExecutorType()); // use a executor type that specify on config file
 	}
 
 	@Test
@@ -133,7 +134,9 @@ public class MybatisAutoConfigurationTest {
 	@Test
 	public void testWithExecutorType() {
 		EnvironmentTestUtils.addEnvironment(this.context,
-				"mybatis.executorType:REUSE");
+				"mybatis.config:mybatis-config.xml");
+		EnvironmentTestUtils.addEnvironment(this.context,
+				"mybatis.executorType:REUSE"); // use a executor type that specify on spring's properties(or yaml)
 		this.context.register(EmbeddedDataSourceConfiguration.class,
 				MybatisAutoConfiguration.class, MybatisMapperConfiguration.class,
 				PropertyPlaceholderAutoConfiguration.class);
