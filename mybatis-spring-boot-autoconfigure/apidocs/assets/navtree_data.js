1fd0807572290c3626d3067c7388ed4f81adba64
Iwao AVE! <harawata@gmail.com>
2016-03-14 20:50:32 +0900

    wagon-git commit

---
+++ mybatis-spring-boot-autoconfigure/apidocs/assets/navtree_data.js
@@ -0,0 +1,7 @@
+var NAVTREE_DATA =
+[ [ "org.mybatis.spring.boot.autoconfigure", "org/mybatis/spring/boot/autoconfigure/package-summary.html", [ [ "Classes", null, [ [ "MybatisAutoConfiguration", "org/mybatis/spring/boot/autoconfigure/MybatisAutoConfiguration.html", null, "" ], [ "MybatisAutoConfiguration.AutoConfiguredMapperScannerRegistrar", "org/mybatis/spring/boot/autoconfigure/MybatisAutoConfiguration.AutoConfiguredMapperScannerRegistrar.html", null, "" ], [ "MybatisAutoConfiguration.MapperScannerRegistrarNotFoundConfiguration", "org/mybatis/spring/boot/autoconfigure/MybatisAutoConfiguration.MapperScannerRegistrarNotFoundConfiguration.html", null, "" ], [ "MybatisProperties", "org/mybatis/spring/boot/autoconfigure/MybatisProperties.html", null, "" ], [ "SpringBootVFS", "org/mybatis/spring/boot/autoconfigure/SpringBootVFS.html", null, "" ] ]
+, "" ] ]
+, "" ] ]
+
+;
+
---
+++ mybatis-spring-boot-autoconfigure/apidocs/reference/lists.js
@@ -0,0 +1,9 @@
+var DATA = [
+      { id:0, label:"org.mybatis.spring.boot.autoconfigure", link:"org/mybatis/spring/boot/autoconfigure/package-summary.html", type:"package" },
+      { id:1, label:"org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration", link:"org/mybatis/spring/boot/autoconfigure/MybatisAutoConfiguration.html", type:"class" },
+      { id:2, label:"org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration.AutoConfiguredMapperScannerRegistrar", link:"org/mybatis/spring/boot/autoconfigure/MybatisAutoConfiguration.AutoConfiguredMapperScannerRegistrar.html", type:"class" },
+      { id:3, label:"org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration.MapperScannerRegistrarNotFoundConfiguration", link:"org/mybatis/spring/boot/autoconfigure/MybatisAutoConfiguration.MapperScannerRegistrarNotFoundConfiguration.html", type:"class" },
+      { id:4, label:"org.mybatis.spring.boot.autoconfigure.MybatisProperties", link:"org/mybatis/spring/boot/autoconfigure/MybatisProperties.html", type:"class" },
+      { id:5, label:"org.mybatis.spring.boot.autoconfigure.SpringBootVFS", link:"org/mybatis/spring/boot/autoconfigure/SpringBootVFS.html", type:"class" }
+
+    ];
