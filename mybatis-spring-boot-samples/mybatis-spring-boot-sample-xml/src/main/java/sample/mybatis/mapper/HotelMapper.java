80318c7def3584dcd153e5ce4accfdf745aebda7
Eduardo Macarron <eduardo.macarron@gmail.com>
2016-04-09 14:14:11 +0200

    Add @Mapper to xml sample

--- mybatis-spring-boot-samples/mybatis-spring-boot-sample-xml/src/main/java/sample/mybatis/mapper/HotelMapper.java
+++ mybatis-spring-boot-samples/mybatis-spring-boot-sample-xml/src/main/java/sample/mybatis/mapper/HotelMapper.java
@@ -16,11 +16,14 @@
 
 package sample.mybatis.mapper;
 
+import org.apache.ibatis.annotations.Mapper;
+
 import sample.mybatis.domain.Hotel;
 
 /**
  * @author Eduardo Macarron
  */
+@Mapper
 public interface HotelMapper {
 
 	Hotel selectByCityId(int city_id);
