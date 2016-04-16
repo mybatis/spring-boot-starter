b38a2a6dee93dd0b00029cb2799e93cdd417085c
Eduardo Macarron <eduardo.macarron@gmail.com>
2016-04-16 19:56:53 +0200

    Minor changes to docs and samples

--- mybatis-spring-boot-samples/mybatis-spring-boot-sample-xml/src/main/java/sample/mybatis/dao/CityDao.java
+++ mybatis-spring-boot-samples/mybatis-spring-boot-sample-xml/src/main/java/sample/mybatis/dao/CityDao.java
@@ -15,7 +15,7 @@
  */
 package sample.mybatis.dao;
 
-import org.mybatis.spring.SqlSessionTemplate;
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -28,10 +28,10 @@ import sample.mybatis.domain.City;
 public class CityDao {
 
 	@Autowired
-	private SqlSessionTemplate sqlSessionTemplate;
+	private SqlSession sqlSession;
 
 	public City selectCityById(long id) {
-		return this.sqlSessionTemplate.selectOne("selectCityById", id);
+		return this.sqlSession.selectOne("selectCityById", id);
 	}
 
 }
