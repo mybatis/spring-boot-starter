db14394f5edc8b1973a9ade770f961c281e5f6e5
Kazuki Shimizu <kazuki43zoo@gmail.com>
2017-01-02 19:24:24 +0900

    Update to MyBatis 3.4.2 and MyBatis-Spring 1.3.1 fix gh-81

--- mybatis-spring-boot-autoconfigure/src/test/java/org/mybatis/spring/boot/autoconfigure/MybatisAutoConfigurationTest.java
+++ mybatis-spring-boot-autoconfigure/src/test/java/org/mybatis/spring/boot/autoconfigure/MybatisAutoConfigurationTest.java
@@ -1,5 +1,5 @@
 /**
- *    Copyright 2015-2016 the original author or authors.
+ *    Copyright 2015-2017 the original author or authors.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -423,7 +423,7 @@ public class MybatisAutoConfigurationTest {
 		this.context.refresh();
 
 		Properties variables = this.context.getBean(SqlSessionFactory.class).getConfiguration().getVariables();
-		assertNull(variables);
+		assertTrue(variables.isEmpty());
 	}
 
 	@Test
