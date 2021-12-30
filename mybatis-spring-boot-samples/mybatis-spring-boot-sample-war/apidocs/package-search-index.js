dd7b6d1d5a7996551261517e41af6d0b954926b5
Iwao AVE! <harawata@gmail.com>
2021-12-30 18:14:52 +0900

    wagon-git commit

--- mybatis-spring-boot-samples/mybatis-spring-boot-sample-war/apidocs/package-search-index.js
+++ mybatis-spring-boot-samples/mybatis-spring-boot-sample-war/apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"sample.mybatis"},{"l":"sample.mybatis.controller"},{"l":"sample.mybatis.domain"},{"l":"sample.mybatis.mapper"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"sample.mybatis.war"},{"l":"sample.mybatis.war.controller"},{"l":"sample.mybatis.war.domain"},{"l":"sample.mybatis.war.mapper"}];updateSearchResults();
