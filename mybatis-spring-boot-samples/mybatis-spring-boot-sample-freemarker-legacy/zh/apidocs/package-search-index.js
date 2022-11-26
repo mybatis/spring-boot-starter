3f3ad0b6643caa09f8e9ead8846c2d29257e33f4
Iwao AVE! <harawata@gmail.com>
2022-11-26 13:01:00 +0900

    wagon-git commit

---
+++ mybatis-spring-boot-samples/mybatis-spring-boot-sample-freemarker-legacy/zh/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"sample.mybatis.freemarker.legacy"},{"l":"sample.mybatis.freemarker.legacy.domain"},{"l":"sample.mybatis.freemarker.legacy.mapper"}];updateSearchResults();
