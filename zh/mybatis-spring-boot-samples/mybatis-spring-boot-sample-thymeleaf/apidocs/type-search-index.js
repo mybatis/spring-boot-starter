991101fa100ad9e3c3a62b368c10a098fc991805
Iwao AVE! <harawata@gmail.com>
2023-11-25 13:34:45 +0900

    wagon-git commit

---
+++ zh/mybatis-spring-boot-samples/mybatis-spring-boot-sample-thymeleaf/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"sample.mybatis.thymeleaf.domain","l":"City"},{"p":"sample.mybatis.thymeleaf.mapper","l":"CityMapper"},{"p":"sample.mybatis.thymeleaf","l":"SampleThymeleafApplication"}];updateSearchResults();
