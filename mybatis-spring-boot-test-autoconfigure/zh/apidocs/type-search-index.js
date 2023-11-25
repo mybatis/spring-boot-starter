58a6edd75c3fb48587ec0d11da68a7134a8bcc59
Iwao AVE! <harawata@gmail.com>
2023-11-25 13:27:54 +0900

    wagon-git commit

---
+++ mybatis-spring-boot-test-autoconfigure/zh/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.mybatis.spring.boot.test.autoconfigure","l":"AutoConfigureMybatis"},{"p":"org.mybatis.spring.boot.test.autoconfigure","l":"MybatisTest"}];updateSearchResults();
