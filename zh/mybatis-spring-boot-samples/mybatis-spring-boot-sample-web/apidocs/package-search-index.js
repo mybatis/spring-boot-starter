d21ee0697802066edaf9f5018327a5a220ae33b4
Iwao AVE! <harawata@gmail.com>
2023-11-25 13:38:41 +0900

    wagon-git commit

---
+++ zh/mybatis-spring-boot-samples/mybatis-spring-boot-sample-web/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"sample.mybatis.web"},{"l":"sample.mybatis.web.controller"},{"l":"sample.mybatis.web.domain"},{"l":"sample.mybatis.web.mapper"}];updateSearchResults();
---
+++ zh/mybatis-spring-boot-samples/mybatis-spring-boot-sample-web/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"sample.mybatis.web.domain","l":"City"},{"p":"sample.mybatis.web.mapper","l":"CityMapper"},{"p":"sample.mybatis.web.controller","l":"CityRestController"},{"p":"sample.mybatis.web","l":"SampleWebApplication"}];updateSearchResults();
